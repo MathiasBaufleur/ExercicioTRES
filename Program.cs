fd4db69ef3f7d1473235d478fda760b430cfbfdb
MathiasBaufleur <baufleurmathias@gmail.com>
2024-04-29 11:22:00 -0300

    terminar
    tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ExercicioTres;
+
+class Program
+{
+    static void Main()
+    {
+        ContaPoupanca contaPoupanca = new ContaPoupanca(1001, 1000, 0.03);
+        ContaCorrente contaCorrente = new ContaCorrente(2001, 2000, 10);
+
+        contaPoupanca.Depositar(300);
+        contaPoupanca.Sacar(150);
+        contaPoupanca.AplicarJuros();
+
+        contaCorrente.Depositar(4500);
+        contaCorrente.Sacar(750);
+
+        Console.WriteLine("Saldo da Conta Poupança: " + contaPoupanca.Saldo);
+        Console.WriteLine("Saldo da Conta Corrente: " + contaCorrente.Saldo);
+    }
+}
+
